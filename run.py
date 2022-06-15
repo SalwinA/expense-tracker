62bea0d884ca645e712bd329d7875e4fbee3f33d
SalwinA <salwino@gmail.com>
2022-06-15 10:35:10 +0000

    Create function to calculate expenses total and update it.

--- run.py
+++ run.py
@@ -17,32 +17,34 @@ SHEET = GSPREAD_CLIENT.open('expense_record')
 def get_expense_data():
     """This function gets the all the expenses data from the user."""
     exp_ls = []
-    year = year_validation('Enter Year (Ex. 2022):', 2020, 2030)
+    year = year_validation('Enter Year (Ex. 2022):', 2020, 2030, 4)
     exp_ls.append(year)
     month = months_and_weeks_validation('Enter Month (Ex. 1 - 12):', 1, 12)
     exp_ls.append(month)
     week_number = months_and_weeks_validation('Enter Week Number (Ex. 1 - 4):', 1, 4)
     exp_ls.append(week_number)
-    food_exp = input('Enter FOOD EXPENSES:')
+    food_exp = expenses_validation('Enter FOOD EXPENSES:', 1, None)
     exp_ls.append(food_exp)
-    car = input('Enter CAR EXPENSES:')
+    car = expenses_validation('Enter CAR EXPENSES:', 0, None)
     exp_ls.append(car)
-    chidlcare = input('Enter CHILDCARE EXPENSES:')
+    chidlcare = expenses_validation('Enter CHILDCARE EXPENSES:', 0, None)
     exp_ls.append(chidlcare)
-    utilities = input('Enter UTILITIES EXPENSES:')
+    utilities = expenses_validation('Enter UTILITIES EXPENSES:', 0, None)
     exp_ls.append(utilities)
-    mortgage = input('Enter MORTGAGE EXPENSES:')
+    mortgage = expenses_validation('Enter MORTGAGE EXPENSES:', 0, None)
     exp_ls.append(mortgage)
-    shopping = input('Enter SHOPPING EXPENSES:')
+    shopping = expenses_validation('Enter SHOPPING EXPENSES:', 0, None)
     exp_ls.append(shopping)
-    ls = [int(i) for i in exp_ls]
-    return ls
+    return exp_ls
 
 
-def year_validation(msg, min=None, max=None):
+def year_validation(msg, min=None, max=None, no_of_digits=None):
     """Years Entry validation"""
     try:
         number = int(input(msg))
+        if no_of_digits:
+            if len(str(number)) != no_of_digits:
+                raise ValueError('Please enter values in 4 digits like 2020....')
         if min and number < min:
             raise ValueError("Year input range should be in the range of {2020 - 2030} ")
         if max and number > max:
@@ -67,6 +69,31 @@ def months_and_weeks_validation(msg, min=None, max=None):
     return months_and_weeks_validation(msg, min, max)
 
 
+def expenses_validation(msg, min=None, max=None):
+    """Month and Weeks Entry validation"""
+    try:
+        number = int(input(msg))
+        if min and number < min:
+            raise ValueError("Input should be greater than " + str(min))
+        if max and number > max:
+            raise ValueError("Input should be less than " + str(max))
+        return number
+    except ValueError as e:
+        print(f'Invalid entry.. {e}. Please try again..\n')
+    return expenses_validation(msg, min, max)
+
+# def expenses_validation(msg, min=None):
+#     """Month and Weeks Entry validation"""
+#     try:
+#         number = int(input(msg))
+#         if min and number < min:
+#             raise ValueError("Input should be greater than " + str(min))
+#         return number
+#     except ValueError as e:
+#         print(f'Invalid entry.. {e}. Please try again..\n')
+#     return expenses_validation(msg, min)
+
+
 def update_worksheet(data1):
     """Update worksheets with data"""    
     print('Updating worksheet...\n')
@@ -76,16 +103,23 @@ def update_worksheet(data1):
     return data1
 
 
+def total_calculation(data):
+    """Calculating expenses total"""
+    dt = data[3:9]
+    sum_of_expenses = sum(dt)
+    return sum_of_expenses
+
+
 def main():
     """This will run all the functions"""
     print()
     print("Welcome to your Expense Tracker\n")
     print("Which function would you like to do today:\n")
-    print(input("A : Enter expense data , B : View past entries, C : Exit\n"))
+    print(input("A : Enter expense data & update, B : View past entries, C : Exit\n"))
     data = get_expense_data()
+    total = total_calculation(data)
+    data.append(total)
     update_worksheet(data)
-    dt = data[3:9]
-    print(dt)
 
 
 main()
